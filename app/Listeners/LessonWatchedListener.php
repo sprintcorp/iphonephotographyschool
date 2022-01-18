dca5cd901089dd760f927e647ea8bdb8b607c601
Frederick Adebayo <sprintcorp7@gmail.com>
2022-01-18 18:50:57 +0100

    lesson watched listener action

--- app/Listeners/LessonWatchedListener.php
+++ app/Listeners/LessonWatchedListener.php
@@ -27,6 +27,13 @@ class LessonWatchedListener
      */
     public function handle(LessonWatched $event)
     {
+        $lesson_watched_achievements = count($event->user->watched);
 
+        $achievement_lesson = [1,5,10,25,50];
+        if(in_array($lesson_watched_achievements,$achievement_lesson)){
+            $achievement = $lesson_watched_achievements > 1 ? $lesson_watched_achievements. ' Lessons Watched' : 'First Lessons Watched';
+            $achievement = new Achievement($event->user,$achievement,'lessons');
+            $achievement->unlock_achievement();
+        }
     }
 }
